03f84b010e583445ef0bb27b786c33b2bf22f1c6
Robert Kaleta <rkaleta02@gmail.com>
2022-01-26 08:36:22 +0100

    Repair ShopingCard and css

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('category/<int:id>', views.ProductList.as_view(), name='list'),
     path('<int:pk>', views.ProductDetail.as_view(), name='detail'),
     path('cart', views.ShoppingCart.as_view(), name='cart'),
+    path('order', views.Order.as_view(), name='order'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,14 +1,16 @@
 import decimal
-
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import HttpResponse
 from home.models import Product
 from home.models import Category
+from home.models import Orders
+from home.models import Order_item
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.shortcuts import get_list_or_404
 from django.views import View
+from home.forms import ShoppingCartOrderForm
 # Create your views here.
 
 class ShoppingCart(View):
@@ -16,21 +18,25 @@ class ShoppingCart(View):
     def _add_cart_item(self, request):
         quantity = request.POST.get('product_quantity')
         id = request.POST.get('id_product')
-        update = request.POST.get('update')
-        print(update)
-        query = render(request, 'home/cart_redirect.html')
-        if request.COOKIES.get(id) and not update:
-            quantity = int(quantity) + int(request.COOKIES.get(id))
+        if request.session.get(id):
+            quantity = int(quantity) + int(request.session.get(id))
             if Product.objects.only('storage').filter(id=id, storage__lt=quantity):
                 return redirect('home:cart')
-        query.set_cookie(id, quantity)
-        return query
+        request.session[id] = quantity
+        return redirect('home:cart')
 
-    def _del_card_item(self, request):
-        cookie_to_del = request.POST.get('del_id')
-        query = render(request,'home/cart_redirect.html')
-        query.delete_cookie(cookie_to_del)
-        return query
+    def _update_cart(self, request):
+        quantity = request.POST.get('product_quantity')
+        id = request.POST.get('id_product')
+        if Product.objects.only('storage').filter(id=id, storage__lt=quantity):
+            return redirect('home:cart')
+        request.session[id] = quantity
+        return redirect('home:cart')
+
+    def _del_cart_item(self, request):
+        session_to_del = request.POST.get('id_product')
+        del request.session[session_to_del]
+        return redirect('home:cart')
 
     def _total_sum(self, cart_product):
         total = decimal.Decimal()
@@ -38,46 +44,76 @@ class ShoppingCart(View):
             total += cart[2]
         return total
 
-    def _del_csrf_token_from_cookie(self, cookie_with_csrf):
-        cookie_clear = {}
-        cookie_clear.update(cookie_with_csrf)
-        cookie_clear.pop('csrftoken')
-        return cookie_clear
-
-    def _bulid_card(self, products, cookie_with_not_csrf):
+    def _bulid_cart(self, products, sess):
         cart_product = []
         for product in products:
             cart_product.append(
                 [
-                    product, cookie_with_not_csrf[str(product.pk)],
-                    decimal.Decimal(cookie_with_not_csrf[str(product.pk)]) * product.price
+                    product, sess[str(product.pk)],
+                    decimal.Decimal(sess[str(product.pk)]) * product.price
                 ]
             )
         return cart_product
 
     def post(self, request):
-        if request.POST.get('del_id'):
-            return ShoppingCart._del_card_item(self, request)
-        else:
+        if request.POST.get('delete'):
+            return ShoppingCart._del_cart_item(self, request)
+        elif request.POST.get('update'):
+            return ShoppingCart._update_cart(self,request)
+        elif request.POST.get('add'):
             return ShoppingCart._add_cart_item(self, request)
 
     def get(self, request):
         id_to_query_sql = []
-        cookie = ShoppingCart._del_csrf_token_from_cookie(self,request.COOKIES)
-        id_to_query_sql.extend(cookie)
+        sess = {}
+        sess.update(request.session)
+        id_to_query_sql.extend(sess)
         products = Product.objects.all().filter(pk__in=id_to_query_sql)
-        cart_product = ShoppingCart._bulid_card(self,products,cookie)
+        cart_product = ShoppingCart._bulid_cart(self,products,sess)
         total_sum_cart = ShoppingCart._total_sum(self,cart_product)
-
+        form = ShoppingCartOrderForm()
         return render(
            request,
            'home/cart.html'
            ,context={
                'menu':Category.objects.only('name'),
-               'cart_product':cart_product,
-               'total':total_sum_cart
+                'cart_product': cart_product,
+                'total': total_sum_cart,
+                'form':form
            }
        )
+
+class Order(View):
+    def post(self, request):
+        items = {}
+        items.update(request.session)
+        form = ShoppingCartOrderForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            order = Orders.objects.create(
+                status ='order_accept',
+                email = data.get('email'),
+                first_name = data.get('first_name'),
+                last_name = data.get('last_name'),
+                post_code = data.get('post_code'),
+                phone_number = data.get('phone_number'),
+                city = data.get('city'),
+                country = data.get('country'),
+                description = data.get('descryption')
+            )
+
+            for id, value in items.items():
+                Order_item.objects.create(
+                    product = Product.objects.all().get(pk=id),
+                    orders = order,
+                    value = value
+            )
+            request.session.clear()
+            return render(request,'home/order.html', context = {'menu':Category.objects.only('name')})
+
+    def get(self, request):
+        return redirect('home:cart')
+
 
 class IndexList(ListView):
     model = Product
@@ -103,7 +139,6 @@ class ProductList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['menu'] = Category.objects.only('name')
-        #context['undo'] = f"category/{self.kwargs.get('id')}"
         return context
 
 class ProductDetail(DetailView):
@@ -111,18 +146,13 @@ class ProductDetail(DetailView):
     template_name = 'home/product_detail.html'
 
     def post(self, request, *args, **kwargs):
-        #print(request.POST)
         return redirect('home:index')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['menu'] = Category.objects.only('name')
-        #context['shoping_cart_form'] = ShoppingCartAddForm()
         return context
 
     def render_to_response(self, context, **response_kwargs):
         response = super(DetailView, self).render_to_response(context, **response_kwargs)
-        #response.set_cookie('sort_on', 67)
-        #print(self.request.COOKIES.get('sort_on'))
-        #foo(response)
         return response
